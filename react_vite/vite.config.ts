921c25840a3ae00300cdf980e014c58715cbec26
ergouHZ <140789367+ergouHZ@users.noreply.github.com>
2024-10-01 23:02:10 +0100

    docker

--- react_vite/vite.config.ts
+++ react_vite/vite.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+  },
   plugins: [react()],
   server: {
     proxy: {
