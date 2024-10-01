dc39fe3f2fb32ed5421c4a9bbefe68da1c638290
ergouHZ <140789367+ergouHZ@users.noreply.github.com>
2024-10-01 22:30:00 +0100

    update root style
    delete redundant style

--- react_vite/src/App.tsx
+++ react_vite/src/App.tsx
@@ -2,8 +2,6 @@ import "./App.css";
 import ChatWindow from "./components/ChatLayout/ChatLayout";
 import { ThemeProviderWrapper } from "./utils/DardThemeContext";
 
-
-
 function App() {
   return (
     <ThemeProviderWrapper>
